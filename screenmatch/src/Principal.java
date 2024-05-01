4b403653f87f257ec74a503861e3f069229b9db1
Alvaro Kieling <atkwaves@gmail.com>
2024-05-01 13:17:12 -0300

    A02 Controlando o acesso e a escrita nos dados da aplicação

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,19 +1,20 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
 
     public static void main(String[] args) {
 
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
 
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
